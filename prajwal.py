94d4879be88c30224c6db2e7981b20348e663687
praj911 <prajwaljayaraj2004@gmail.com>
2025-12-10 17:34:37 +0530

    corrected code logic

--- prajwal.py
+++ prajwal.py
@@ -1,11 +1,11 @@
 def get_grade(score):
-    if score >= 90:
+    if 90 < score <= 100:
         return "A"
-    elif score >= 80:
+    elif 80 < score <=90:
         return "B"
-    elif score >= 70:
+    elif 70 < score <=80:
         return "C"
-    elif score >= 60:
+    elif 60 < score <= 70:
         return "D"
     else:
         return "F"
